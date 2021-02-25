6d4ca99c8de5f53fba3b90c59c3b648ffecbcd70
Sam <sam@Sams-MacBook-Air.local>
2021-02-24 20:21:10 -0500

    commit tree 69e6512e0669fbe822ae29455dc3a75bfb2cc3de

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        pig = PigLatinizer.new
+        @piglatin = pig.piglatinize(params[:user_phrase])
+        erb :results
+    end
 end
